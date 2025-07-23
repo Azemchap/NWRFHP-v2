51a4875f1312cea9b06248538ea17a67d3fb9ffa
ndehdonatus <ndehdonatus@gmail.com>
2025-07-23 11:26:36 +0200

    made changes on contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -44,20 +44,20 @@ const ContactUs = () => {
                                         </p>
                                         <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                                             <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
-                                            <Link
-                                                   href="https://wa.me/+2376111222333"
+                                                <Link
+                                                    href="https://wa.me/+237651421052"
                                                     className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-normal rounded-md shadow-sm text-white bg-blue-600 bg-opacity-90 hover:bg-opacity-80 sm:px-8 gap-1"
                                                 >
-                                                 <FaWhatsapp />  Chat on Whatsapp
+                                                    <FaWhatsapp />  Chat on Whatsapp
                                                 </Link>
 
                                                 <Link
-                                                   rel="noopener" target="_blank" href="tel:+2376111222333"
+                                                    rel="noopener" target="_blank" href="tel:+237651421052"
                                                     className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-normal rounded-md shadow-sm text-blue-600 bg-white hover:bg-indigo-50 sm:px-8 gap-2"
                                                 >
-                                                  <FaPhoneVolume /> Make a phone call
+                                                    <FaPhoneVolume /> Make a phone call
                                                 </Link>
-                                               
+
                                             </div>
                                         </div>
                                     </div>
