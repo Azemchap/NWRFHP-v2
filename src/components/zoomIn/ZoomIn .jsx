e8320237e53946eee2a82427fe348af1f1592c60
ndehdonatus <ndehdonatus@gmail.com>
2025-07-25 16:19:24 +0200

    added request form link on contact page

--- src/components/zoomIn/ZoomIn .jsx
+++ src/components/zoomIn/ZoomIn .jsx
@@ -259,7 +259,7 @@ function App() {
 
                         <Image className='img'
                             alt="slider image" width={500} height={500}
-                            src="/images/nfor blaise.jpg"
+                            src="/images/ blaise.jpg"
                         />
 
                     </div>
