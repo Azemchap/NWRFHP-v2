f252311f95534fe7665ac30dde2249c0fc9b8387
ndehdonatus <109814763+ndehdonatus@users.noreply.github.com>
2025-11-15 11:21:29 +0100

    updates some hero icons

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
-import Image from 'next/image'
-import sideImg from "../../../public/images/096A0583.jpg"; // Import the image
+import Image from "next/image";
+const sideImg = "/images/096A0583.jpg"; // Import the image
 
 const AboutUs = () => {
   return (
@@ -23,13 +23,20 @@ const AboutUs = () => {
           <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
             <div className="lg:col-start-2 lg:pl-8">
               <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
-                <h2 className="leading-6 text-blue-600 font-semibold tracking-wide uppercase">About us</h2>
+                <h2 className="leading-6 text-blue-600 font-semibold tracking-wide uppercase">
+                  About us
+                </h2>
                 <div>
                   <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     Who are we?
                   </h3>
                   <p className="mt-8 text-lg text-gray-500">
-                    The North West Regional Fund for Health Promotion - PIG is a public corporate dialogue structure set up by a constituent agreement of the state, the technical/financial partners, and the North West community on June 11, 2012 in line with law no. 2010/023 of December 2010 to define the status of Public Interest Groups in Cameroon.
+                    The North West Regional Fund for Health Promotion - PIG is a
+                    public corporate dialogue structure set up by a constituent
+                    agreement of the state, the technical/financial partners,
+                    and the North West community on June 11, 2012 in line with
+                    law no. 2010/023 of December 2010 to define the status of
+                    Public Interest Groups in Cameroon.
                   </p>
                 </div>
                 <div className="mt-8">
@@ -37,14 +44,40 @@ const AboutUs = () => {
                     Brief history
                   </h3>
                   <p className="mt-8 text-lg text-gray-500">
-                    The North West Regional Fund for Health Promotion - PIG was set up in 1987 as North West Pro Pharmacy to ensure a constant supply of quality essential medicines to the population of the North West region in line with the 5th principal concerns of the Bamako Initiative. In 1991 it was transformed into North West Provincial Special Fund for Health in line with Law No.90/62 of 19th December 1990 relating to Freedom of associations. To improve its services to the population, it was transformed into a Public Interest Group on June 11, 2012 in line with Law No.2010/023 of December 2010 on Public Interest Groups in Cameroon. In compliance with its constituent agreement approved by Prime Ministerial order no. 005/CAB/PM of January 21, 2013 a Constitutive General Assembly was held in December 2013 to put in place the Management Committee and adopt the internal regulations and organizational chart.
+                    The North West Regional Fund for Health Promotion - PIG was
+                    set up in 1987 as North West Pro Pharmacy to ensure a
+                    constant supply of quality essential medicines to the
+                    population of the North West region in line with the 5th
+                    principal concerns of the Bamako Initiative. In 1991 it was
+                    transformed into North West Provincial Special Fund for
+                    Health in line with Law No.90/62 of 19th December 1990
+                    relating to Freedom of associations. To improve its services
+                    to the population, it was transformed into a Public Interest
+                    Group on June 11, 2012 in line with Law No.2010/023 of
+                    December 2010 on Public Interest Groups in Cameroon. In
+                    compliance with its constituent agreement approved by Prime
+                    Ministerial order no. 005/CAB/PM of January 21, 2013 a
+                    Constitutive General Assembly was held in December 2013 to
+                    put in place the Management Committee and adopt the internal
+                    regulations and organizational chart.
                   </p>
                 </div>
                 <div className="mt-5 prose prose-indigo text-gray-500">
                   <h3 className="mt-5 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                    How we’re different</h3>
+                    How we’re different
+                  </h3>
                   <p className="mt-8 text-lg text-gray-500">
-                    As PIG, it is a public corporate dialogue structure of the state, the technical/financial partners and the North West Community. As an essential health care program based on practical, scientifically sound, and socially acceptable methods, its services are made accessible to the population through their full participation and at a cost they can afford. Starting with 59 Community Pharmacies, about 100 Village Health Posts (now Integrated Health Centres) and a geographical coverage of 90%, it supplies medicines to 217 community pharmacies in public health facilities covering a population of 2.2 million inhabitants.
+                    As PIG, it is a public corporate dialogue structure of the
+                    state, the technical/financial partners and the North West
+                    Community. As an essential health care program based on
+                    practical, scientifically sound, and socially acceptable
+                    methods, its services are made accessible to the population
+                    through their full participation and at a cost they can
+                    afford. Starting with 59 Community Pharmacies, about 100
+                    Village Health Posts (now Integrated Health Centres) and a
+                    geographical coverage of 90%, it supplies medicines to 217
+                    community pharmacies in public health facilities covering a
+                    population of 2.2 million inhabitants.
                   </p>
                 </div>
               </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 
-import { ArrowRightIcon } from '@heroicons/react/outline'
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import Carousel from './Carousel'
 
